37e03fd95741345c4dc8fd208ffb3cbc16ae3883
Tryps <86132393+DMTryptamines@users.noreply.github.com>
2023-03-15 08:23:17 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
-Blog::Engine.routes.draw do
-  resources :secrets do
-  end
-  post "secrets/perform_show" => "secrets#perform_show"
-  get "/" => "topics#index"
-  get "posts" => "topics#index"
-  get "sitemap.xml" => "topics#sitemap"
-  get "about" => "blog#about"
-  get "robots.txt" => "robots#index"
-  get "*path" => "topics#permalink"
+MyPluginModule::Engine.routes.draw do
+  get "/examples" => "examples#index"
+  # define routes here
 end
+
+Discourse::Application.routes.draw { mount ::MyPluginModule::Engine, at: "my-plugin" }
